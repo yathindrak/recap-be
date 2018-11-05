40e493e3aaf79ad85f1b835b27274f744261e9ee
Yathindra Kodithuwakku <yathindrarawya123@gmail.com>
2018-11-05 10:56:35 +0530

    Removed dist folder

--- dist/schema/board.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = "\n\n  type Board {\n    id: ID\n    name: String,\n    description: String,\n    columns: [Column]\n  }\n\n   type BoardResponse {\n    ok: Boolean!\n    board: Board\n    errors: [Error!]\n  }\n  \n  type Query {\n    getBoards: [Board]\n    getBoard(id: Int!): Board\n  }\n  \n  type Subscription {\n    boardAdded: Board\n    boardEdited: Board\n    boardDeleted: Board\n  }\n\n  type Mutation {\n    createBoard(name: String!, description: String!): BoardResponse!\n    updateBoard(id: Int!, name: String!, description: String!): BoardResponse!\n    deleteBoard(id: Int!): BoardResponse!\n  }\n";
--- dist/schema/card.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = "\n\n  type Card {\n    id: ID,\n    name: String,\n    description: String,\n    order_num: Int\n  }\n\n   type CardResponse {\n    ok: Boolean!\n    card: Card\n    errors: [Error!]\n  }\n  \n  type Query {\n    getCards: [Card]\n    getCard(id: Int!): Card\n  }\n  \n  type Subscription {\n    cardAdded: Card\n    cardEdited: Card\n    cardDeleted: Card\n  }\n  \n  type Mutation {\n    createCard(columnId: Int!, name: String!, description: String!, order_num: Int!): CardResponse!\n    updateCard(id: Int!, name: String!, description: String!): CardResponse!\n    deleteCard(id: Int!): CardResponse!\n    rearrangeCard(id: Int!, columnId: Int!, order_num: Int!): CardResponse!\n  }\n";
